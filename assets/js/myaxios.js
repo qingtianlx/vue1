91a69397ac5dbcee2f528ebb7ba947fc8e9a0f71
lixin <1369860690@qq.com>
2019-02-02 11:28:54 +0800

    Create myaxios.js

---
+++ assets/js/myaxios.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+var myaxios = {}
+
+myaxios.install = function (Vue) {
+  axios.defaults.baseURL = 'http://localhost:8888/api/private/v1'
+  // 添加一个拦截器
+  axios.interceptors.request.use(function (config) {
+    if (config.url !== '/login') {
+      config.headers['Authorization'] = window.localStorage.getItem('token')
+    }
+    return config
+  })
+  Vue.prototype.$http = axios
+}
+export default myaxios
