2df9bbf900c518635bc09671b0b435de8dae94f0
lixin <1369860690@qq.com>
2019-02-02 11:23:30 +0800

    Create bread.vue

---
+++ components/bread.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-breadcrumb separator-class="el-icon-arrow-right">
+    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+    <el-breadcrumb-item>{{first}}</el-breadcrumb-item>
+    <el-breadcrumb-item>{{last}}</el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {
+  name: 'bread',
+  props: ['first', 'last']
+}
+</script>
+
+<style>
+
+</style>
